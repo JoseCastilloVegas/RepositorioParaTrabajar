3288cef8b91d90a2f2523431e8ec012fbbb07d06
Jose Antonio Castillo Vegas <josecastillovegas@hotmail.com>
2025-07-09 13:47:28 +0200

    Conflicto merge 2

--- src/main/java/com/example/Prueba_Test/SoyUnaClaseDePrueba.java
+++ src/main/java/com/example/Prueba_Test/SoyUnaClaseDePrueba.java
@@ -4,5 +4,5 @@ public class SoyUnaClaseDePrueba {
 
     String ejemplo2="Soy el ejemplo 2";
 
-    String a="Soy el commit de la clase de prueba hija para el merge";
+    String a="Soy el commit de la clase de prueba hija para el merge 2";
 }
